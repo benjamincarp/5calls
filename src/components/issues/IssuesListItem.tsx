d4b501ca5fc1193e75e8634b13e9765a3e056450
Ben Carpenter <ben.carpenter@bottlerocketstudios.com>
2020-06-29 15:42:34 -0500

    update styling on circles, needs some refactoring

--- src/components/issues/IssuesListItem.tsx
+++ src/components/issues/IssuesListItem.tsx
@@ -33,7 +33,11 @@ export class IssuesListItem extends React.Component<Props, State> {
             aria-live="polite"
             className={`issues-list__item__status ${isCompleted} ${isActive}`}
           >
-            <span className="visually-hidden" />
+            {isCompleted ? (
+              <span className="visually-hidden" />
+            ) : (
+              getProgressCircle(isIssueActive, completeCount, contactsCount)
+            )}
           </span>
           <span
             className={`issues-list__item__title ${isCompleted} ${isActive}`}
@@ -50,6 +54,40 @@ export class IssuesListItem extends React.Component<Props, State> {
       </li>
     );
   }
+}
+
+function getProgressCircle(
+  isIssueActive: boolean,
+  completeCount: number,
+  contactsCount: number
+) {
+  const R = 18;
+  const C = 113; // 2 * Pi * R
+  const dashLength = (C * completeCount) / contactsCount;
+  const width = 4;
+  const circleStyle1 = {
+    fill: 'transparent',
+    stroke: '#e5e5e5',
+    strokeWidth: `${width}`
+  };
+  const circleStyle2 = {
+    fill: 'transparent',
+    stroke: '#2ab371',
+    strokeWidth: `${width}`,
+    strokeDasharray: `${dashLength} ${C - dashLength}`,
+    strokeDashoffset: `${C / 4}`
+  };
+
+  return (
+    <svg
+      viewBox={`0 0 ${R * 2} ${R * 2}`}
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ overflow: 'visible' }}
+    >
+      <circle cx={`${R}`} cy={`${R}`} r={`${R}`} style={circleStyle1} />
+      <circle cx={`${R}`} cy={`${R}`} r={`${R}`} style={circleStyle2} />
+    </svg>
+  );
 }
 
 export default IssuesListItem;
